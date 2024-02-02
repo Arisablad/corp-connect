178ede73061617bffa4322e613494ef0b6d4ae14
Remigiusz Szostak <145459815+Arisablad@users.noreply.github.com>
2024-02-02 17:10:53 +0100

    deployment fixes

--- src/components/shared/ReviewProgressBar.tsx
+++ src/components/shared/ReviewProgressBar.tsx
@@ -4,11 +4,11 @@ const ReviewProgressBar = ({ count = 5 }: { count?: number }) => {
   return (
     <div className="w-28 rounded-xl p-1.5 flex gap-0.5">
       {Array.from({ length: count }).map((_, i) => {
-        return <div className="bg-green-400 w-full rounded-md"></div>;
+        return <div key={i} className="bg-green-400 w-full rounded-md"></div>;
       })}
 
       {Array.from({ length: emptyDivs }).map((_, i) => {
-        return <div className="bg-orange-400 w-full rounded-md"></div>;
+        return <div key={i} className="bg-orange-400 w-full rounded-md"></div>;
       })}
     </div>
   );
